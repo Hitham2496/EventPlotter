1e563124b96b5e47d9c078aa4a2d20e6b01db96d
Hitham Hassan <hitham.t.hassan@durham.ac.uk>
2022-08-20 20:42:19 +0100

    Add documented, refactored particle, event classes

---
+++ src/EventPlotter/event.py
@@ -0,0 +1,121 @@
+from EventPlotter import particle
+import numpy as np
+
+
+class Event():
+
+    def __init__(
+                 self, particles, root_s: float = 0., mur: float = 0.,
+                 muf: float = 0., set_info: bool = True, x_plus: float = 0.,
+                 x_minus: float = 0., wgt: float = 1.
+                ):
+        """
+        Saves information of particles in an event and sets
+        additional information of event by default. Arguments:
+            particles: array-like container of EventPlotter Particles
+            root_s: float, sqrt CoM energy of interaction
+            mur: float, renormalisation scale
+            muf: float, factorisation scale
+            set_info: bool, sets the x+/- values of the incoming particles and
+                      the CoM energy of the hard interaction
+            x_plus: float, x value of +z incoming particle
+            x_minus: float, x value of -z incoming particle
+            wgt: float, event weight
+        """
+        self.particles = particles
+        self.root_s = root_s
+        self.mur = mur
+        self.muf = muf
+        self.wgt = wgt
+        self.find_incoming()
+        self.check_momentum()
+        if (set_info):
+            self.setup()
+            return
+
+        self.x_plus = x_plus
+        self.x_minus = x_minus
+
+    def find_incoming(self):
+        """
+        Finds the positions of the incoming particles in the particles
+        container.
+        """
+        incoming = [-1, -1]
+        beams = [-1, -1]
+        for idx, part in enumerate(self.particles):
+            if (part.is_incoming() and part.e() == self.root_s / 2.):
+                if (part.pz() > 0.):
+                    beams[0] = idx
+                elif (part.pz() < 0.):
+                    beams[1] = idx
+            elif ((part.is_incoming()) and (part.perp() == 0.)
+                  and (part.pz() != 0.) and (part.e() != 0.)):
+                side = int(part.pz() / np.abs(part.pz()))
+                if (side == 1):
+                    incoming[0] = idx
+                elif (side == -1):
+                    incoming[1] = idx
+                # else:
+                #   raise(ValueError("Incoming particle"))
+            if (incoming[0] != -1 and incoming[1] != -1):
+                self.incoming = incoming
+                return
+        if (incoming[0] == -1 or incoming[1] == -1):
+            raise(ValueError("One or more incoming particles was not found"))
+
+    def setup(self):
+        """
+        Calculates global quantities of event including:
+        centre of mass energy, pdf energy fractions. This
+        method should be called if any modification to the
+        particle content has been applied after init.
+        """
+        self.root_s_hat = self.calc_root_s_hat()
+        self.x_plus = self.calc_x(1)
+        self.x_minus = self.calc_x(-1)
+
+    def calc_root_s_hat(self):
+        """
+        Calculates the hard interaction CoM energy of an event.
+        """
+        inc = self.incoming
+        p_ini = self.particles[inc[0]].momentum
+        p_ini += self.particles[inc[1]].momentum
+        if (p_ini.p_z == 0. or p_ini.e == 0. or p_ini.p_t != 0):
+            raise(ValueError("""Event does not contain incoming particles
+                             of valid momentum."""))
+
+        return p_ini.m
+
+    def calc_x(self, direction: int):
+        """
+        Calculates the x value for each particle incoming along the beam axis,
+        admits the direction (+1 for positive z, -1 for negative z) as a
+        parameter. Assumes equal energy beams.
+        """
+        inc = self.incoming
+        if (direction == 1):
+            return self.particles[inc[0]].e() / (self.root_s / 2.)
+        elif (direction == -1):
+            return self.particles[inc[1]].e() / (self.root_s / 2.)
+        else:
+            raise(ValueError("""The direction must be +/-1 for the incoming
+                             particles for the +/- z axis respectively."""))
+
+    def check_momentum(self, tol: float = 1E-2):
+        """
+        Checks momentum conservation in the event to within a tolerance set to
+        1E-2 by default.
+        """
+        inc = self.incoming
+        EPS = 1E-2
+        p_check = self.particles[inc[0]].momentum
+        p_check += self.particles[inc[1]].momentum
+        for idx, part in enumerate(self.particles):
+            if (part.is_final()):
+                p_check -= part.momentum
+
+        for comp in p_check:
+            if (abs(comp) > EPS):
+                raise(ValueError("""Momentum not conserved in event"""))
---
+++ src/EventPlotter/particle.py
@@ -0,0 +1,120 @@
+import numpy as np
+import pylorentz
+from typing import Tuple
+
+
+class Particle():
+
+    def __init__(
+            self, pdg: int = 0, status: int = 0,
+            mothers: Tuple[int, int] = (0, 0),
+            daughters: Tuple[int, int] = (0, 0),
+            cols: Tuple[int, int] = (0, 0),
+            px: float = 0., py: float = 0., pz: float = 0., e: float = 0.,
+            m: float = 0., check_on_shell: bool: True
+            ):
+        """
+        Initialise particle from provided data, most of which is optional.
+            pdg: id of particle according to pdg database convention
+            status: status code of particle following the Pythia convention
+            mothers: tuple containing index of mother particles if applicable
+            daughters: tuple containing index of daughter particles if applicable
+            cols: tuple containing col,anticol values for partons if provided
+            px, py, pz, e: floats, momentum components for particles
+            m: float, mass of the particle, not as calculated from its momentum
+            check_on_shell: bool, whether to check if provided particle is on-shell
+        """
+        self.pdg = pdg
+        self.status = status
+        self.mothers = mothers
+        self.daughters = daughters
+        self.cols = cols
+        self.momentum = pylorentz.Momentum4(e, px, py, pz)
+        self.m = m
+        if (check_on_shell):
+            self.check_on_shell()
+
+    def momentum(self):
+        """
+        Returns the momentum of the particle.
+        """
+        return self.momentum
+
+    def m_calc(self):
+        """
+        Calculate mass from momentum.
+        """
+        p = self.momentum
+        return np.sqrt(p.e()**2 - p.px()**2 - p.py()**2 - p.pz()**2)
+
+    def rap(self):
+        """
+        Wrapper around rapidity form pylorentz.
+        """
+        p = self.momentum
+        return 0.5 * np.log((p.e() + p.p_z)/(p.e() - p.p_z))
+
+    def phi(self):
+        """
+        Wrapper around phi form pylorentz.
+        """
+        p = self.momentum
+        return np.arctan2(p.p_y, p.p_x)
+
+    def perp(self):
+        """
+        Wrapper around p_T form pylorentz.
+        """
+        p = self.momentum
+        return np.sqrt(p.p_x**2 + p.p_y**2)
+
+    def px(self):
+        """
+        Wrapper around p_x
+        """
+        return self.momentum.p_x
+
+    def py(self):
+        """
+        Wrapper around p_x
+        """
+        return self.momentum.p_y
+
+    def pz(self):
+        """
+        Wrapper around p_x
+        """
+        return self.momentum.p_z
+
+    def e(self):
+        """
+        Wrapper around p_x
+        """
+        return self.momentum.e
+
+    def is_parton(self):
+        """
+        Compares the pdg id code from init to a container of parton pdg codes.
+        """
+        return (abs(self.pdg) in [1, 2, 3, 4, 5, 6, 21])
+
+    def is_final(self):
+        """
+        Returns true if the (Pythia) status code of the particle is positive.
+        """
+        return (self.status > 0)
+
+    def is_incoming(self):
+        """
+        Returns true if the (Pythia) status code of the particle is negative.
+        """
+        return (self.status < 0)
+
+    def check_on_shell(self, tol: float = 1E-3):
+        """
+        Checks on-shellness of the particle compared to the init mass to
+        within a tolerance set to 1E-3 by default.
+        """
+        calc = self.m_calc()
+        if(np.abs((calc - self.m()) / self.m()) > tol):
+            raise(ValueError("Particle is not on shell"))
