77a49853948d21c4abce59e5e0f4a9e39c9e523a
Hitham Hassan <hitham.t.hassan@durham.ac.uk>
2022-09-18 17:16:00 +0100

    Produce tests for multiple configurations of particles in test_event

--- test/test_event.py
+++ test/test_event.py
@@ -2,38 +2,53 @@ import pytest
 import numpy as np
 from EventPlotter import Particle, Event
 
-p2 = Particle(2212, status=-1, px=0., py=0., pz=7.e+03, e=7e+03)
+p2 = Particle(2212, status=-1, px=0., py=0., pz=3.5e+03, e=3.5e+03)
 
-p1 = Particle(2212, status=-1, px=0., py=0., pz=7.e+03, e=7e+03)
+p1 = Particle(2212, status=-1, px=0., py=0., pz=3.5e+03, e=3.5e+03)
 
-A = Particle(21, status=-1, px=0., py=0., pz=1.0978691649e+02, e=1.0978691649e+02)
+A = Particle(3, status=-1, px=0., py=0., pz=80.387, e=80.387)
 
-B = Particle(21, status=-1, px=0., py=0., pz=-4.9113387232e+01, e=4.9113387232e+01)
+B = Particle(1, status=-1, px=0., py=0., pz=-435.476, e=435.476)
 
-C = Particle(21, status=1, px=7.1902798158e+00, py=-6.7312608086e+01, pz=-4.440162797e-01, e=6.7697004968e+01, m=0.)
+C = Particle(2, status=1, px=125.233, py=40.134, pz=-315.891, e=342.17125, m=0.)
 
-D = Particle(21, status=1, px=-7.1902798158e+00, py=+6.7312608086e+01, pz=-4.440162797e-01, e=6.7697004968e+01, m=0.)
+D = Particle(21, status=1, px=-0.469, py=1.953, pz=-4.284, e=4.731, m=0.)
 
-E = Particle(21, status=1, px=3.0123136796e+01, py=7.05490777531e+01, pz=-1.3763091874e+02, e=1.5756536909e+02, m=0.)
+E = Particle(3, status=1, px=-52.286, py=-22.262, pz=-5.853, e=57.129, m=0.206)
+
+F = Particle(-12, status=1, px=-65.701, py=-41.757, pz=-38.699, e=86.936, m=0.11)
+
+G = Particle(11, status=1, px=-6.777, py=21.931, pz=9.638, e=24.896, m=0.15)
 
 
-event_balanced_beams = Event([p1, p2, A,B,C,D], 7000)
-event_balanced = Event([A,B,C,D], 7000)
-#event_unbalanced = Event([A,B,C,D,E], 7000)
+event_balanced_beams = Event([p1, p2, A, B, C, D, E, F, G], 7000)
+event_balanced_beams = Event([p1, p2, A, B, C, D, E, F, G], 7000, set_info=False,
+                             x_plus=0.02297, x_minus=0.1244)
+event_balanced = Event([A, B, C, D, E, F, G], 7000)
 #event_no_incoming = Event([C,D], 7000)
 
 
+@pytest.mark.parametrize("event", [
+    (event_balanced_beams),
+    (event_balanced),
+])
 def test_momentum_balanced(event: Event):
-    assert check_momentum(event) == True
+    assert event.check_momentum() == True
+    assert isinstance(str(event), str)
 
 
-def test_incoming(event: Event):
-    # beams, incoming found, exceptions raised
-    return
+def test_momentum_val_error():
+    with pytest.raises(ValueError):
+        event_unbalanced = Event([A,B,C,D,E], 7000)
+
+    with pytest.raises(ValueError):
+        event_unbalanced = Event([p2,p1,C,D,E], 7000)
 
 
-def test_set_info(event: Event, x_plus_true: float, x_minus_true: float):
-    return
-
-
+def test_reorder():
+    # check reordering event is valid
+    event_reorder = Event([p1, p2, B, C, A, D, E, F, G], 7000)
+    event_reorder_beams = Event([p2, p1, B, C, A, D, E, F, G], 7000)
+    assert event_reorder.check_momentum() == True
+    assert event_reorder_beams.check_momentum() == True
 
