d7bb2dd2f578c69980d6468b8e4af3a2815b6517
Hitham Hassan <hitham.t.hassan@durham.ac.uk>
2022-09-24 18:31:44 +0100

    Integrate structure of Plotter class

--- src/EventPlotter/plotter.py
+++ src/EventPlotter/plotter.py
@@ -3,8 +3,10 @@ from matplotlib import cm
 from matplotlib.colors import ListedColormap, LinearSegmentedColormap
 import numpy as np
 from typing import Tuple
-from particle import Particle
-
+from .particle import Particle
+from .event import Event
+import sys
+np.set_printoptions(threshold=sys.maxsize)
 
 class Plotter():
 
@@ -17,11 +19,11 @@ class Plotter():
                  chosen_map: str = "YlOrRd",
                  min_val: int = 1,
                  custom_z_axis = None,
-                 z_label: str = r"$p_\perp$"
+                 z_label: str = r"$p_\perp \quad [\textrm{GeV}]$",
                  include_wgt: bool = False
                 ):
         """
-        Initialise plotter class, arguments:
+        Initialise Plotter class, arguments:
             rap_extent: tuple for the limits along the rapidity axis.
             phi_extent: tuple for the limits along the phi axis.
             bins: number of bins along each axis.
@@ -38,7 +40,7 @@ class Plotter():
         self.rap_extent = sorted(rap_extent)
         self.phi_extent = sorted(phi_extent)
         self.bins = int(bins)
-        self.z_function = custom_z_axis
+        self.z_func = custom_z_axis
         self.z_label = z_label
         self.include_wgt = include_wgt
         self.get_colormap(chosen_map, min_val)
@@ -56,64 +58,89 @@ class Plotter():
         new_colors = set_map(np.linspace(0, 1, 256))
         white = np.array([256/256, 256/256, 256/256, 1])
         new_colors[:int(min_val), :] = white
-        self.color_map = ListedColormap(newcolors)
+        self.color_map = ListedColormap(new_colors)
 
 
-    def get_image(self, event: Event = None):
+    def get_image(self, event: Event = None, idx_products = None):
         """
         Returns a square numpy array of dimension bins*bins, populated with the
         z-axis values corresponding to each point in rapidity-azimuthal angle
-        space.
+        space. Option to provide an array or array-like container of indices for
+        decay products (or other particles not to be included in the heatmap)
+        as 'idx_products'.
         """
         y_phi = []
-        y_phi_products = []
-        for idx, p in enumerate(event.particles):
-            if (p.is_final() and p.is_parton()):
-                y_phi.append([idx, p.rap(), p.phi(), p.perp()])
-            elif (p.is_final() and (not p.is_parton()) and (not np.abs(p.status) > 62)):
-                y_phi_products.append([p.pdg, p.rap(), p.phi(), p.perp()])
+        if (not idx_products):
+            for idx, p in enumerate(event.particles):
+                if (p.is_final() and p.is_parton()):
+                    fill_val = self.z_func(p) if (self.z_func) else p.perp()
+                    fill_val *= event.wgt if (self.include_wgt) else 1
+                    y_phi.append([idx, p.rap(), p.phi(), fill_val])
+        else:
+            for idx, p in enumerate(event.particles):
+                if (p.is_final() and p.is_parton()
+                    and (idx not in idx_products)):
+                    fill_val = float(self.z_func(p)) if (self.z_func) else p.perp()
+                    fill_val *= event.wgt if (self.include_wgt) else 1
+                    y_phi.append([idx, p.rap(), p.phi(), fill_val])
 
         y_phi.sort(key = lambda x: x[3])
-        y_phi_products.sort(key = lambda x: x[3])
 
         image = np.zeros((self.bins, self.bins))
 
         for j, part in enumerate(y_phi):
-            y = np.abs(np.linspace(self.phi_extent[0], self.phi_extent[1],
+            phi = np.abs(np.linspace(self.phi_extent[0], self.phi_extent[1],
                        self.bins) - part[2]).argmin()
-            x = np.abs(np.linspace(self.rap_extent[0], self.rap_extent[1],
+            rap = np.abs(np.linspace(self.rap_extent[0], self.rap_extent[1],
                        self.bins) - part[1]).argmin()
             if ((part[1] > self.rap_extent[0] and part[1] < self.rap_extent[1])
                 and (part[2] > self.phi_extent[0] and part[2] < self.phi_extent[1])):
-                image[y,x] += part[3]
+                image[phi, rap] += part[3]
 
-        return [image, y_phi_products]
+        if (idx_products):
+            y_phi_products = []
+            for loc in idx_products:
+                p = event.particles[loc]
+                fill_val = self.z_func(p) if (self.z_func) else p.perp()
+                fill_val *= event.wgt if (self.include_wgt) else 1
+                y_phi_products.append([p.pdg, p.rap(), p.phi(), fill_val])
+
+            y_phi_products.sort(key = lambda x: x[3])
+            return image, y_phi_products
 
 
-    def plot_y_phi(self, image: np.array, products = 0, title="event.pdf"):
+        return image
+
+
+    def plot_y_phi(self, image, products = None, title: str = "event.pdf"):
         """
-        Renders a 3D lego plot of the y-phi plane
+        Renders a square heatmap of the y-phi plane given:
+            image: a (bins x bins) dimensional numpy array
+            products: (optional) array-like container [id,y,phi,fill_value]
+                      for particles to be displayed as scatter points
+            title: title of the figure to be saved, by default 'event.pdf'
         """
-
+        if (image is None):
+            raise(ValueError("Must provide an image to plot"))
         plt.rc('text', usetex=True)
         plt.rc('font', family='serif')
         plt.xlabel(r'$y$')
         plt.ylabel(r'$\phi \quad [\textrm{rad}]$')
 
-        plt.imshow(image, interpolation='none', origin='lower', aspect=4.4/3.14, extent=[-4.4, 4.4, -3.14, 3.14],
-                   cmap=newcmp)
+        rap_length = np.abs(self.rap_extent[0] - self.rap_extent[1])
+        phi_length = np.abs(self.phi_extent[0] - self.phi_extent[1])
+        plt.imshow(image, interpolation='none', origin='lower', aspect=rap_length/phi_length,
+                   extent=[self.rap_extent[0], self.rap_extent[1], self.phi_extent[0],
+                           self.phi_extent[1]], cmap=self.color_map)
 
         bar = plt.colorbar()
-        if ( not (isinstance(products,int)) ):
+        if (products is not None):
             for pt in products:
-                plt.scatter([pt[1]], [pt[2]], marker='x', color='black')
-                if (round(abs(pt[0])) == 11):
-                    plt.annotate(r"$e^-$", (pt[1]+0.1, pt[2]+0.1))
-                elif (round(abs(pt[0])) == 12):
-                    plt.annotate(r"$\overline{\nu}$", (pt[1]+0.1, pt[2]+0.1))
+                plt.scatter(pt[1], pt[2], c="black", marker='x')
+                plt.annotate(r"id="+str(round(np.abs(pt[0]))), (pt[1]+0.1, pt[2]+0.1))
         # bar.ax.yaxis.set_ticks([point[3] for point in y_phi], minor=True)
 
-        bar.set_label(r'$p_\perp \quad [\textrm{GeV}]$')
+        bar.set_label(self.z_label)
         plt.savefig(title, bbox_inches='tight')
         plt.clf()
         return image
