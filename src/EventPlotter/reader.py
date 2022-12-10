562c7ae3b369800e11d4b922a50a1f683c896fce
Hitham Hassan <hitham.t.hassan@durham.ac.uk>
2022-12-10 22:05:53 +0000

    Complete implementation of buffer in LHE reader

--- src/EventPlotter/reader.py
+++ src/EventPlotter/reader.py
@@ -37,23 +37,48 @@ class ReaderLHEF(Reader):
 
         self.buffer = ET.iterparse(filename, events = ("start", "end") )
         self.current_event = None
+        self.init_info = None
 
     def read_next(self, verbose: bool = False):
         """
         Advances the reader by one event.
         """
         xml_event = next(self.buffer)
+        wgts = None
 
+        # Write initialisation information if provided in LHE file
         while xml_event[1].tag != "event":
+            if xml_event[0] == "end" and xml_event[1].tag == "init":
+                self.init_info = xml_event[1].text.strip().split("\n")
+
             xml_event = next(self.buffer)
 
+        # Look only at events
         while not (xml_event[1].tag == "event" and xml_event[0] == "end"):
+            # Keep weights if provided
+            if xml_event[1].tag == "weights" and xml_event[0] == "end":
+                wgts = [float(x) for x in xml_event[1].text.strip().split()]
+
             xml_event = next(self.buffer)
 
         text_event = xml_event[1].text.strip().split("\n")
-        print(text_event)
 
+        parts = []
+        for idx, line in enumerate(text_event[1:]):
+            data = line.split()
+            parts.append(Particle(pdg = int(data[0]),
+                                  status = int(data[1]),
+                                  cols = (int(data[4]), int(data[5])),
+                                  px = float(data[6]),
+                                  py = float(data[7]),
+                                  pz = float(data[8]),
+                                  e = float(data[9]),
+                                  m = float(data[10]),
+                                  check_on_shell = False
+                                  )
+                        )
 
+        return Event(particles = parts, set_info = False)
 
 
 class ReaderPythia(Reader):
