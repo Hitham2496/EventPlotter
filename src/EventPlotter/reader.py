f05ed7f57ef5d6a509f7249b1b8e1bfd10bce12d
Hitham Hassan <hitham.t.hassan@durham.ac.uk>
2022-12-10 23:13:48 +0000

    Create beams from init info

--- src/EventPlotter/reader.py
+++ src/EventPlotter/reader.py
@@ -14,7 +14,7 @@ class Reader():
         Initialises an event reader object from the event file `filename`.
         """
 
-    def read_next(self, verbose: bool = False):
+    def read_next(self):
         """
         Advances the reader by one event.
         """
@@ -27,10 +27,12 @@ class Reader():
 
 class ReaderLHEF(Reader):
 
-    def __init__(self, filename: str):
+    def __init__(self, filename: str, wgt_idx: int = None):
         """
         Initialises a LHE reader object from the event file `filename` ending in .lhe,
         we use the xml implementation of the LHE format to simplify te implementation.
+        Optionally add a weight index to initialise the events with weights as provided
+        in the file.
         """
         if not str(filename).endswith(".lhe"):
             raise(ValueError("Event file must end with the .lhe extension."))
@@ -38,8 +40,11 @@ class ReaderLHEF(Reader):
         self.buffer = ET.iterparse(filename, events = ("start", "end") )
         self.current_event = None
         self.init_info = None
+        self.wgt_idx = wgt_idx
+        self.beams = []
+        self.com_energy = 0.
 
-    def read_next(self, verbose: bool = False):
+    def read_next(self):
         """
         Advances the reader by one event.
         """
@@ -49,11 +54,11 @@ class ReaderLHEF(Reader):
         # Write initialisation information if provided in LHE file
         while xml_event[1].tag != "event":
             if xml_event[0] == "end" and xml_event[1].tag == "init":
-                self.init_info = xml_event[1].text.strip().split("\n")
+                self.set_init_info(xml_event[1].text)
 
             xml_event = next(self.buffer)
 
-        # Look only at events
+        # Look only at events for event record
         while not (xml_event[1].tag == "event" and xml_event[0] == "end"):
             # Keep weights if provided
             if xml_event[1].tag == "weights" and xml_event[0] == "end":
@@ -64,6 +69,10 @@ class ReaderLHEF(Reader):
         text_event = xml_event[1].text.strip().split("\n")
 
         parts = []
+        # Add particles to event container, starting with system and beams
+        parts.append(Particle())#status=-1, e = self.init_info[2]+self.init_info[3]))
+        parts.append(self.beams[0])
+        parts.append(self.beams[1])
         for idx, line in enumerate(text_event[1:]):
             data = line.split()
             parts.append(Particle(pdg = int(data[0]),
@@ -78,7 +87,34 @@ class ReaderLHEF(Reader):
                                   )
                         )
 
-        return Event(particles = parts, set_info = False)
+        if not self.wgt_idx is None:
+            return Event(particles = parts,
+                         root_s = self.com_energy,
+                         set_info = False,
+                         wgt = wgts[self.wgt_idx]
+                        )
+
+        return Event(particles = parts, root_s = self.com_energy, set_info = False)
+
+    def set_init_info(self, info: str = None):
+        """
+        Set info from initialisation of LHE file incl. beams given
+        the xml data in string format.
+        """
+        if self.init_info is None:
+            init_info = [float(i) for i in info.strip().split("\n")[0].split()]
+            self.beams.append(Particle(pdg = int(init_info[0]),
+                                       status = -1,
+                                       pz = init_info[2],
+                                       e = init_info[2])
+                             )
+            self.beams.append(Particle(pdg = int(init_info[1]),
+                                       status = -1,
+                                       pz = -init_info[3],
+                                       e = init_info[3])
+                             )
+            self.com_energy = self.beams[0].E() + self.beams[1].E()
+            self.init_info = init_info
 
 
 class ReaderPythia(Reader):
