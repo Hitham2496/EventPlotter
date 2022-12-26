e8b3a0f71a3e65f59319b5c050ffcdbcf70d7e0f
Hitham Hassan <hitham.t.hassan@durham.ac.uk>
2022-12-26 19:06:33 +0000

    Use correct path for data in unit tests

--- test/test_reader.py
+++ test/test_reader.py
@@ -8,18 +8,14 @@ init_info = [2212.0, 2212.0, 3500.0, 3500.0, 0.0, 0.0,
 
 def test_incorrect_ext():
     with pytest.raises(ValueError):
-        reader = ReaderLHEF("HEJFOG.lhe.gz", wgt_idx=0)
+        reader = ReaderLHEF("test/HEJFOG.lhe.gz", wgt_idx=0)
 
 
 def test_reading():
-    os.system("gunzip HEJFOG.lhe.gz")
-    reader = ReaderLHEF("HEJFOG.lhe", wgt_idx=0)
+    os.system("gunzip test/HEJFOG.lhe.gz")
+    reader = ReaderLHEF("test/HEJFOG.lhe", wgt_idx=0)
     assert reader.init_info == init_info
     for idx, ev in enumerate(reader):
         assert isinstance(ev, Event)
 
-
-
- #   iter_ev = iter(reader)
-
-    os.system("gzip HEJFOG.lhe")
+    os.system("gzip test/HEJFOG.lhe")
